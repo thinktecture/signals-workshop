9f89a03b2271ea894e916ae450cbf409d78b030a
Felix Schütz <felix.schuetz@thinktecture.com>
2025-02-19 16:50:03 +0100

    refactor: set change detection to OnPush

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { Counter } from "./counter/counter";
 
 @Component({
@@ -6,5 +6,6 @@ import { Counter } from "./counter/counter";
   imports: [Counter],
   templateUrl: "./app.html",
   styleUrl: "./app.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {}
--- src/app/counter/counter.ts
+++ src/app/counter/counter.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 let id = 1;
 
@@ -20,6 +20,7 @@ const nthColor = colorPicker([
   imports: [],
   templateUrl: "./counter.html",
   styleUrls: ["./counter.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Counter {
   private readonly storageKey = `counter${id++}`;
