9ee083193188bc3b6bd4a3747761c1c0bc40d5a0
Felix Schütz <felix.schuetz@thinktecture.com>
2025-02-20 07:24:15 +0100

    refactor: use signal for counter

--- src/app/counter/counter.ts
+++ src/app/counter/counter.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 let id = 1;
 
@@ -14,32 +14,31 @@ const nthColor = colorPicker(['#d98324', '#a40606', '#0f4c5c', '#6c9a8b', '#c1d7
   imports: [],
   templateUrl: './counter.html',
   styleUrls: ['./counter.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Counter {
   private readonly storageKey = `counter${id++}`;
 
   private readonly restored = Number(sessionStorage.getItem(this.storageKey));
 
-  protected counter = this.restored || 0;
+  protected readonly counter = signal(this.restored || 0);
 
-  protected color(): string {
+  protected readonly color = computed(() => {
     console.log('running', this.storageKey);
-    return nthColor(this.counter);
+    return nthColor(this.counter());
+  });
+
+  constructor() {
+    effect(() => {
+      sessionStorage.setItem(this.storageKey, String(this.counter()));
+      console.log('persisted', this.storageKey);
+    });
   }
 
   protected increment(): void {
-    this.counter = this.counter + 1;
-    this.persist();
+    this.counter.update((counter) => counter + 1);
   }
 
   protected reset(): void {
-    this.counter = 0;
-    this.persist();
-  }
-
-  private persist(): void {
-    sessionStorage.setItem(this.storageKey, String(this.counter));
-    console.log('persisted', this.storageKey);
+    this.counter.set(0);
   }
 }
