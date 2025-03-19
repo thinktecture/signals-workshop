4b638242dee3b3a1f830dba2d55d43591c72f9e5
Felix Schütz <felix.schuetz@thinktecture.com>
2025-03-19 17:34:12 +0100

    feat: add checkbox to toggle persisting

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,26 +1,70 @@
-import { Component, effect, signal, WritableSignal } from "@angular/core";
+import {
+  Component,
+  effect,
+  EffectRef,
+  inject,
+  Injector,
+  Signal,
+  signal,
+  untracked,
+  WritableSignal,
+} from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { Counter } from "./counter/counter";
 
 function createCounter(storageKey: string): WritableSignal<number> {
   const counter = signal(Number(sessionStorage.getItem(storageKey) || 0));
   console.log("loaded", storageKey);
 
-  effect(() => {
-    sessionStorage.setItem(storageKey, String(counter()));
-    console.log("persisted", storageKey);
-  });
-
   return counter;
+}
+
+function persist(
+  counter: Signal<number>,
+  storageKey: string,
+  injector: Injector,
+): EffectRef {
+  return effect(
+    () => {
+      sessionStorage.setItem(storageKey, String(counter()));
+      console.log("persisted", storageKey);
+    },
+    { injector },
+  );
 }
 
 @Component({
   selector: "app-root",
-  imports: [Counter],
+  imports: [Counter, FormsModule],
   templateUrl: "./app.html",
   styleUrl: "./app.scss",
 })
 export class App {
+  protected readonly persist = signal(true);
+
   protected readonly counter1 = createCounter("counter1");
   protected readonly counter2 = createCounter("counter2");
   protected readonly counter3 = createCounter("counter3");
+
+  private persist1?: EffectRef;
+  private persist2?: EffectRef;
+  private persist3?: EffectRef;
+
+  private readonly injector = inject(Injector);
+
+  constructor() {
+    effect(() => {
+      if (this.persist()) {
+        untracked(() => {
+          this.persist1 = persist(this.counter1, "counter1", this.injector);
+          this.persist2 = persist(this.counter2, "counter2", this.injector);
+          this.persist3 = persist(this.counter3, "counter3", this.injector);
+        });
+      } else {
+        this.persist1?.destroy();
+        this.persist2?.destroy();
+        this.persist3?.destroy();
+      }
+    });
+  }
 }
